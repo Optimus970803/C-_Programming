1e212d21bb6fac817b4bc91c6d4be6cd86c39001
unknown <experte001@gmail.com>
2024-10-10 18:21:53 +0900

    Doubly Linked List Implementation and Remove, append,prepend and print methods

---
+++ JS/DSA-with-ZTM/Session-7/108.DoublyLinkedList.js
@@ -0,0 +1,97 @@
+class DoublyLinkedList {
+  constructor(value) {
+    this.head = {
+      value: value,
+      prev: null,
+      next: null,
+    };
+    this.tail = this.head;
+    this.length = 1;
+  }
+
+  append(value) {
+    const newNode = {
+      value: value,
+      prev: null,
+      next: null,
+    };
+    newNode.prev = this.tail;
+    this.tail.next = newNode;
+    this.tail = newNode;
+    this.length++;
+    return this;
+  }
+  prepend(value) {
+    const newNode = {
+      value: value,
+      prev: null,
+      next: null,
+    };
+    newNode.next = this.head;
+    this.head.prev = newNode;
+    this.head = newNode;
+    this.length++;
+    return this;
+  }
+  printList() {
+    const array = [];
+    let currentNode = this.head;
+    while (currentNode !== null) {
+      array.push(currentNode.value);
+      currentNode = currentNode.next;
+    }
+    return console.log(array);
+  }
+  insert(index, value) {
+    //Check for proper parameters;
+    if (index >= this.length) {
+      console.log("yes");
+      return this.append(value);
+    }
+
+    const newNode = {
+      value: value,
+      prev: null,
+      next: null,
+    };
+    const perviousNode = this.traverseToIndex(index - 1);
+    const currentNode = perviousNode.next;
+
+    perviousNode.next = newNode;
+    newNode.prev = perviousNode;
+    newNode.next = currentNode;
+    currentNode.prev = newNode;
+    this.length++;
+    return this.printList();
+  }
+  traverseToIndex(index) {
+    //Check parameters
+    let counter = 0;
+    let currentNode = this.head;
+    while (counter !== index) {
+      currentNode = currentNode.next;
+      counter++;
+    }
+    return currentNode;
+  }
+  remove(index) {
+    // Check Parameters
+    const perviousNode = this.traverseToIndex(index - 1);
+
+    const currentNode = perviousNode.next;
+
+    perviousNode.next = currentNode.next;
+    perviousNode.next.prev = currentNode;
+    this.length--;
+    return this.printList();
+  }
+}
+
+let myLinkedList = new DoublyLinkedList(10);
+myLinkedList.append(5);
+myLinkedList.append(16);
+myLinkedList.prepend(1);
+myLinkedList.printList();
+myLinkedList.insert(2, 99);
+myLinkedList.insert(20, 88);
+myLinkedList.remove(2);
