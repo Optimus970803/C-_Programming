bbd06db0d6e756bb4dd8f63d447a7bb6f83f572e
unknown <experte001@gmail.com>
2025-01-11 01:25:30 +0900

    Searching

---
+++ JS/DSA-with-ZTM/Section-14/JavaScriptSearching.js
@@ -0,0 +1,5 @@
+const beasts = ["Centaur", "Godzilla", "Mosura", "Minotaur", "Hydra", "Nessie"];
+console.log(beasts.indexOf("Godzilla"));
+console.log(beasts.findIndex((item) => item === "Godzilla"));
+console.log(beasts.find((item) => item === "Godzilla"));
+console.log(beasts.includes("Godzilla"));
